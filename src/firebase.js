4d048b6737e5311e1660b61a6da097322323dc56
davidbirch4272 <davidbirch4272@gmail.com>
2025-05-14 10:55:59 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB516UEKKaxE-AkNtEDZqU_0j8Ynik4d48",
+  authDomain: "netflix-clone10021972.firebaseapp.com",
+  projectId: "netflix-clone10021972",
+  storageBucket: "netflix-clone10021972.firebasestorage.app",
+  messagingSenderId: "699244149462",
+  appId: "1:699244149462:web:c64a181089d6492a8d6777"
+};
